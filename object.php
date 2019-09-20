b29294301d3ae3dbb376b6fa9ab41d42f85fafa3
MirkoPieri <mirko97.pieri@gmail.com>
2019-09-20 18:15:10 +0200

    Inserimenti dati utente via Url.

--- object.php
+++ object.php
@@ -19,27 +19,39 @@
       $area = 2 * ($this -> lunghezza * $this -> altezza + $this -> lunghezza * $this -> profondita + $this -> altezza * $this -> profondita);
 
       return $area;
-    }
+    } //funzione per calcolare area
 
     public function getVol() {
 
       $vol = $this -> lunghezza * $this -> altezza * $this -> profondita;
 
       return $vol;
-    }
+    } //funzione per calcolare volume
   }
 
+  //prendo dati da URL
+  $x = $_GET['x'];
+  $y = $_GET['y'];
+  $z = $_GET['z'];
 
+  $x1 = $_GET['x1'];
+  $y1 = $_GET['y1'];
+  $z1 = $_GET['z1'];
+
+  $x2 = $_GET['x2'];
+  $y2 = $_GET['y2'];
+  $z2 = $_GET['z2'];
+
+//creo array oggetti
   $parallelepipedi = [
-    new Parallelepipedo(3, 4, 5),
-    new Parallelepipedo(3, 4, 5),
-    new Parallelepipedo(3, 4, 5),
-    new Parallelepipedo(3, 4, 5)
+    new Parallelepipedo($x, $y, $z),
+    new Parallelepipedo($x1, $y1, $z1),
+    new Parallelepipedo($x2, $y2, $z2)
   ];
 
   $sumArea = 0;
   $sumVol = 0;
-
+  //stampo tutte le formule
   foreach ($parallelepipedi as $key) {
     echo 'Area: ' . $key -> getArea() . '<br>';
     echo 'Volume: ' . $key -> getVol() . '<br><br>';
@@ -47,7 +59,7 @@
     $sumArea += $key -> getArea();
     $sumVol += $key -> getVol();
   }
-
+  //stampo la somma di voliume e area
   echo "Somma aree: " . $sumArea . '<br>';
   echo "Somma volumi: " . $sumVol . '<br>';
  ?>
